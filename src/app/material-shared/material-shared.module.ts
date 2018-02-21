a2c57ff1b63f4abaf543a2e06e0534edd806052b
Ernesto Jiménez Villaseñor <ernesto@bedu.org>
2018-02-21 09:27:29 -0600

    Added angular material shared module

---
+++ src/app/material-shared/material-shared.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { MatInputModule } from "@angular/material/input";
+import { MatToolbarModule } from "@angular/material/toolbar";
+
+@NgModule({
+  imports: [MatInputModule, MatToolbarModule],
+  exports: [MatInputModule, MatToolbarModule],
+  declarations: []
+})
+export class MaterialSharedModule { }
